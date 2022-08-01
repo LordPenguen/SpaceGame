7aca2d1dd5a090af4c4d9fa3961f6c916a36dbc2
Elif Gümüştaş <elif_gumus_tas@hotmail.com>
2022-08-01 21:01:03 +0300

    Particle
    Particle sistemini araştırdım, prefab şeklinde kullanmayı öğrendim.

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
 
 public class Enemies : MonoBehaviour
 {
-      private void OnCollisionEnter2D(Collision2D other) {
-        Destroy(gameObject);
-    }
+
+  [SerializeField] ParticleSystem particle;
+
+
+
+  void OnCollisionEnter2D(Collision2D other) {
+
+    Instantiate(particle,transform.position,Quaternion.identity);
+    Destroy(gameObject);
+    
+  }
+
     
 }
