d0faf77dce7ea3be53686e5a4422a6f4ee53c026
Elif Gümüştaş <elif_gumus_tas@hotmail.com>
2022-08-04 20:23:02 +0300

    bullet ui
    Bulletları ilk sınırlı sayıda yaptım (4). Sonra ui ekledim, böylelikle kaç mermii kladığı gözüküyor.

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,8 +6,20 @@ public class Shooting : MonoBehaviour
     public Transform firepoint;
     public GameObject bulletPrefab;
 
+    [SerializeField] private GameObject[] ammo;
+
+    private int counter=0;
+
     public float bulletForce = 1f;
-    
+
+    void Start() {
+
+        for(int i=0; i<4; i++){
+
+            ammo[i].gameObject.SetActive(true);
+        }
+        
+    }
     
     void Update()
     {
@@ -19,11 +31,16 @@ public class Shooting : MonoBehaviour
 
     void Shoot(){
 
+        if(counter<4){
+
         GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
 
-    }
+        ammo[counter].gameObject.SetActive(false);
+        counter++;
+        }
 
+    }
       
 }
